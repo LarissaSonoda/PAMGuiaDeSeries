10d9cd1226a43dee27db8a4b4e59b2a3bd3d89ec
LarissaSonoda <larissa.sonoda@etec.sp.gov.br>
2021-06-10 09:14:27 -0300

    3 séries adicionadas a tela inicial logo inserido menu de navegação inferior adicionado 09/06/21

---
+++ app/src/main/java/com/example/pamguiaseries/WandaVision.java
@@ -0,0 +1,14 @@
+package com.example.pamguiaseries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WandaVision extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wanda_vision);
+    }
+}
