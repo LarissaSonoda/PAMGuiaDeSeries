b323732b6c59a691f907e504c6e32a94c6d94b92
LarissaSonoda <larissa.sonoda@etec.sp.gov.br>
2021-06-11 18:29:24 -0300

    Tela de informações de WandaVision Intent para acessar cada uma das activities

--- app/src/main/java/com/example/pamguiaseries/WandaVision.java
+++ app/src/main/java/com/example/pamguiaseries/WandaVision.java
@@ -2,13 +2,20 @@ package com.example.pamguiaseries;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WandaVision extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wanda_vision);
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
