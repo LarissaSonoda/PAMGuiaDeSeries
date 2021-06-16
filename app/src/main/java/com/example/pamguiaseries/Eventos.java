3b2aaa1764ab9a133964f35606e5e1966d816476
LarissaSonoda <larissa.sonoda@etec.sp.gov.br>
2021-06-16 18:40:39 -0300

    commit 16/06 finalização da tela com sinopse, trailer, comentário e avaliação

--- app/src/main/java/com/example/pamguiaseries/Eventos.java
+++ app/src/main/java/com/example/pamguiaseries/Eventos.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Eventos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_eventos);
     }
-    public void abrirSerie(View view){
-        Intent intent = new Intent(this, WandaVision.class);
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/pamguiaseries/WandaVision.java
+++ app/src/main/java/com/example/pamguiaseries/WandaVision.java
@@ -3,6 +3,7 @@ package com.example.pamguiaseries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,7 +16,16 @@ public class WandaVision extends AppCompatActivity {
         setContentView(R.layout.activity_wanda_vision);
     }
     public void sendMessage(View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        Intent voltar = new Intent(this, MainActivity.class);
+        startActivity(voltar);
+    }
+
+    public void abrirTrailer(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/sj9J2ecsSpo"));
+        startActivity(Intent.createChooser(browserIntent, "Abra o trailer"));
+    }
+    public void abrirDisney(View view){
+        Intent watchSeries = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.disneyplus.com/pt-br/series/wandavision/4SrN28ZjDLwH"));
+        startActivity(Intent.createChooser(watchSeries, "Abrir player"));
     }
 }
